2aabf735878e9de9fc3380aa395025d7ccab43c5
ElinaBaht <elina.bahtegaryaeva@gmail.com>
2022-07-07 11:31:38 +0300

    redux_filter

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
+
 import { Routes, Route } from 'react-router-dom';
 export const SearchContext = React.createContext('');
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
+
   return (
     <div className="wrapper">
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
