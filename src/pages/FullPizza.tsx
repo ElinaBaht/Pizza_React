a7c77dd825676a04ce734baaa7868207a6e7fc3c
ElinaBaht <elina.bahtegaryaeva@gmail.com>
2022-07-14 12:25:35 +0300

    tsx

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom';
 
-const FullPizza = () => {
-  const [pizza, setPizza] = React.useState();
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+  }>();
   const { id } = useParams();
   const navigate = useNavigate();
   React.useEffect(() => {
@@ -20,13 +24,12 @@ const FullPizza = () => {
   }, []);
 
   if (!pizza) {
-    return 'Загрузка...';
+    return <>Загрузка...</>;
   }
   return (
     <div className="container">
       <img src={pizza.imageUrl} alt="Pizza" />
       <h2>{pizza.title}</h2>
-
       <h4>{pizza.price} р</h4>
     </div>
   );
